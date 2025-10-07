fdffc041fa5495bb538cb93d801ad1a6392fbb1b
Ryan Hansen <ryan.hansen@octanner.com>
2025-10-07 11:17:00 -0600

    wip

--- terragrunt/environments/qa/main.tf
+++ terragrunt/environments/qa/main.tf
@@ -20,3 +20,11 @@ variable "environment" {
 variable "common" {
   type = any
 }
+
+module "rds" {
+  source = "git@github.com:ketiko/terraform-modules.git//rds?ref=main"
+}
+
+module "s3" {
+  source = "git@github.com:ketiko/terraform-modules.git//s3?ref=main"
+}
